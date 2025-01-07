d143a9d2cd38e2d342248622afd8bec14b1f1022
yusufalqard <yusuf.alqard@gmail.com>
2025-01-07 17:13:44 +0700

    add: basic operation numeric code javascript

---
+++ javascript-room/primitive-data.js
@@ -0,0 +1,13 @@
+// Primitive data types in JavaScript
+// There are Number, String, Boolean, Undefined, Null, Symbol, and BigInt
+
+// Example of Number
+// let bookline = 1 + 2;
+// Result are 1 + 2 = 3
+// let bookline = 1 - 2;
+// Result are 1 - 2 = -1
+// let bookline = 1 * 2;
+// Result are 1 * 2 = 2
+let bookline = 1 / 2;
+// Result are 1 / 2 = 0.5
+console.log(bookline);
