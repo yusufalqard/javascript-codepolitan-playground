9a20a4e88f6ab435ed448b7cbaa07fb1c2081208
yusufalqard <yusuf.alqard@gmail.com>
2025-01-07 17:22:44 +0700

    rev: example of num and add example of string

--- javascript-room/primitive-data.js
+++ javascript-room/primitive-data.js
@@ -8,6 +8,18 @@
 // Result are 1 - 2 = -1
 // let bookline = 1 * 2;
 // Result are 1 * 2 = 2
-let bookline = 1 / 2;
+// let bookline = 1 / 2;
 // Result are 1 / 2 = 0.5
-console.log(bookline);
+// console.log(bookline);
+// const phi = 22 / 7;
+// let bookline = 7 * phi ;
+// Result are 7 * 22 / 7 = 22
+// console.log(bookline);
+// let bookline = 2 ** 5;
+// Result are 2 ** 5 = 32
+// console.log(bookline);
+
+// Example of String
+const greet = "Hello, ";
+const MYname = "Yusuf Al Qardhawi";
+console.log(greet + MYname);
