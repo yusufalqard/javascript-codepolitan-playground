63c28d42131426ea1debe4907b031937b85e649a
yusufalqard <yusuf.alqard@gmail.com>
2025-01-09 05:07:05 +0700

    add: var-data script and explanation

---
+++ javascript-room/variable-data.js
@@ -0,0 +1,16 @@
+// There is Constants and Variables in JavaScript
+// Their fundamental difference is that Constants cannot be changed, while Variables can be changed
+// Let function is used to declare Variables, while Const function is used to declare Constants
+
+// Example of Constants
+const YourScore = 90;
+// YourScore += 1;
+// YourScore -= 1;
+// YourScore *= 1;
+// YourScore /= 1;
+console.log(YourScore);
+// Result are 90 even though there are 4 math operations, const function cannot be changed or modified
+// Enable/Disable the code above to see the result with Quokka.js
+// There is an error message that says "TypeError: Assignment to constant variable." in each math operation
+
+
