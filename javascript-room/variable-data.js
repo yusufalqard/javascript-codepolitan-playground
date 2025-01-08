fcf86cdc3d9c476be7dda544e8fa0f66aaf29c33
yusufalqard <yusuf.alqard@gmail.com>
2025-01-09 05:15:42 +0700

    add: let func script and explanation

--- javascript-room/variable-data.js
+++ javascript-room/variable-data.js
@@ -8,9 +8,20 @@ const YourScore = 90;
 // YourScore -= 1;
 // YourScore *= 1;
 // YourScore /= 1;
+// YourScore++;
 console.log(YourScore);
 // Result are 90 even though there are 4 math operations, const function cannot be changed or modified
-// Enable/Disable the code above to see the result with Quokka.js
+// Enable/Disable each math operation to see the result
 // There is an error message that says "TypeError: Assignment to constant variable." in each math operation
 
-
+// Example of Variables using Let function
+let MyGrade = 90;
+MyGrade += 1;
+// MyGrade -= 1;
+// MyGrade *= 2;
+// MyGrade /= 2;
+// MyGrade++;
+console.log(MyGrade);
+// Result are different from the Constants, MyGrade can be changed or modified
+// Enable/Disable each math operation to see the result
+// There is no error message because let function can be changed or modified
