e933501e7d9ec2f92b2c71136e301fba0e411112
yusufalqard <yusuf.alqard@gmail.com>
2025-01-09 15:53:49 +0700

    add: operator comparison code

---
+++ javascript-room/logical-decision.js
@@ -0,0 +1,25 @@
+// There is logical expression in javascript
+// Operator Comparison are commonly used by developer
+// Here an example how to use it
+let calculation = 1 > 2;
+// let calculation = 1 < 2;
+// let calculation = 1 >= 2;
+// let calculation = 1 <= 2;
+// let calculation = 1 == 2;
+// let calculation = 1 != 2;
+// let calculation = 1 === 2;
+// let calculation = 1 !== 2;
+console.log(calculation);
+// Enable/Disable each code comparison to see the result
+// For documentation, you can access by click this link https://unicode.org/charts/PDF/U0000.pdf
+// You can also do comparison not just numeric, but also string, conditional, etc
+
+// Double and Triple equal mostly used for comparison of value
+// Here an example how to use it
+A = 3 == '3';
+console.log(A);
+B = 3 === '3';
+console.log(B);
+// Difference between double and triple equal is double equal only compare the value not the data type
+// While triple equal compare the value and the data type
+// 
