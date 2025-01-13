bea10a6206dd187131ff1bd2912262237f9670c9
yusufalqard <yusuf.alqard@gmail.com>
2025-01-13 11:19:08 +0700

    add: example code for debug in browser console

--- javascript-room/logical-decision.js
+++ javascript-room/logical-decision.js
@@ -11,7 +11,7 @@ let calculation = 1 > 2;
 // let calculation = 1 !== 2;
 console.log(calculation);
 // Enable/Disable each code comparison to see the result
-// For documentation, you can access by click this link https://unicode.org/charts/PDF/U0000.pdf
+// For documentation, you can access by visit this link https://unicode.org/charts/PDF/U0000.pdf
 // You can also do comparison not just numeric, but also string, conditional, etc
 
 // Double and Triple equal mostly used for comparison of value
@@ -22,4 +22,16 @@ B = 3 === '3';
 console.log(B);
 // Difference between double and triple equal is double equal only compare the value not the data type
 // While triple equal compare the value and the data type
-// 
+// Logical decision sometimes can be difficult to understand how to make our program works
+// But with practice study case and experience by doing from failure, bug, error, etc
+// Your code will much better later on
+
+// Most common javascript code for debug
+// There is log(), alert(), warn(), error(), prompt(), confirm(), etc
+// Here an example how to use it
+console.log('This is logging');
+console.warn('Warning');
+console.error('Error');
+prompt('Fill this message below');
+confirm('Are you sure?');
+// Use browser console to see the result
