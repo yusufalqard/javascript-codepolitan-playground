6f43a5a19e0e1d02980d1c66a882ac184ef8a3a4
yusufalqard <yusuf.alqard@gmail.com>
2025-01-13 12:00:40 +0700

    add: If else statement code

---
+++ javascript-room/logic-playground.js
@@ -0,0 +1,19 @@
+// This is example javascript code from logical-decision.js
+
+// console.log('This is logging');
+// console.warn('Warning');
+// console.error('Error');
+// prompt('Fill this message below');
+// confirm('Are you sure?');
+
+// Enable/Disable code one by one to see the result
+// Install Live Server Extension to make your development work project easier
+
+// IF ELSE STATEMENT
+let Age = 26; // Change the value to see different result
+if (Age > 18) {
+    console.log('You are adult');
+} else {
+    console.log('You are not adult');
+}
+// Check the result in browser console or using Quokka.js
