ccc24716941cd097306e73e0a7cd50d96357d10f
yusufalqard <yusuf.alqard@gmail.com>
2025-01-16 05:15:36 +0700

    add: change array value line 11

--- javascript-room/array-data.js
+++ javascript-room/array-data.js
@@ -8,3 +8,14 @@ console.log(box[1]);
 // Array index start from 0
 // Enable and Disable code one by one to see the result
 
+// Example how to change array value
+let color = ['red', 'green', 'blue'];
+color[1] = 'yellow';
+color[2] = 'green';
+console.log(color);
+// Result: ['red', 'yellow', 'green']
+// Because we change the value of index 1 and 2
+// Array always start from 0,1,2,...,n which n is the last index
+// You can easily remember this Array[n] as reference
+// If we give const for function of color, we can't override the value
+// Unlike let where you can freely change value data everytime
