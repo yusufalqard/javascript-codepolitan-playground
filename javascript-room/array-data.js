600498c9a28a56d64303ca9d755601e093aeb8ad
yusufalqard <yusuf.alqard@gmail.com>
2025-01-15 17:52:08 +0700

    add: array data script

---
+++ javascript-room/array-data.js
@@ -0,0 +1,10 @@
+// Array Data Structure
+let box = ['apple', 'banana', 'lemon'];
+// console.log(box);
+// console.log(box[0]);
+console.log(box[1]);
+// console.log(box[2]);
+// Array is a data structure that can store multiple values
+// Array index start from 0
+// Enable and Disable code one by one to see the result
+
