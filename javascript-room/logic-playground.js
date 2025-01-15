e88863d8a3251544e774c037c86b60127fa51518
yusufalqard <yusuf.alqard@gmail.com>
2025-01-15 17:44:41 +0700

    add: switch case statement line 90

--- javascript-room/logic-playground.js
+++ javascript-room/logic-playground.js
@@ -86,3 +86,31 @@ if (UserLogIN !== 'admin') {
 } else {
   console.log('Welcome admin');
 }
+
+// Using Switch Case Statement
+const Day = prompt("Enter the day name");
+switch (Day) {
+  case 'Monday':
+    console.log('Today is Monday');
+    break;
+  case 'Tuesday':
+    console.log('Today is Tuesday');
+    break;
+  case 'Wednesday':
+    console.log('Today is Wednesday');
+    break;
+  case 'Thursday':
+    console.log('Today is Thursday');
+    break;
+  case 'Friday':
+    console.log('Today is Friday');
+    break;
+  case 'Saturday':
+    console.log('Today is Saturday');
+    break;
+  case 'Sunday':
+    console.log('Today is Sunday');
+    break;
+  default:
+    console.log('Invalid day');
+}
