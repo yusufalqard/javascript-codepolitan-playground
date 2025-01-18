b8e0cbb00102d7f16a78a1bcc6bf92386ecbac41
yusufalqard <yusuf.alqard@gmail.com>
2025-01-18 13:07:47 +0700

    add: array example from line 62 to 127

--- javascript-room/array-data.js
+++ javascript-room/array-data.js
@@ -48,3 +48,80 @@ console.log(NumTicket);
 // Array shift means that you will remove item on start of array index
 // Array unshift means that you will add item on start of array index
 // Result are [43,23,203,105,319,51,3]
+
+// There are some other array method you can use
+// concat - for combining array of index
+// includes - for search value inside of array with condition true or false
+// indexOf - searching index of array position
+// join - make string from some array
+// reverse - make array sort by descending
+// slice - copy or replace some value from specific array of index
+// splice - add remove or delete of array element
+// sort - for sorting of value array
+
+// Array concat
+let Melon = [12,41,31,99];
+let Lemon = [80,19,22,60];
+const fruit = Lemon.concat(Melon);
+console.log(fruit);
+// Result are 80, 19, 22, 60, 12, 41, 31,99
+// This is how array concat work
+
+// Array sort
+fruit.sort();
+console.log(fruit);
+// Result are 12, 19, 22, 32, 41, 60, 80, 99 
+// This is how array sort work
+
+// Array Reverse
+fruit.reverse();
+console.log(fruit);
+// Result are 99, 80, 60, 41, 31, 22, 19, 12
+// This is how array reverse work
+
+
+// Array indexOf
+const Animal = ['Elephant','Fish','Shark','Piranha'];
+console.log(Animal.indexOf('Elephant'));
+// console.log(Animal.indexOf('Fish'));
+// console.log(Animal.indexOf('Shark'));
+console.log(Animal.indexOf('Piranha'));
+// Result are index of 0, 1, 2, and 3
+// Enable/Disable the code of Animal in console.log
+
+// Array includes
+console.log(Animal.includes('Fish'));
+console.log(Animal.includes('Cat'));
+// Result are true because fish exist on array and false because cat does not exist
+// This is how array includes work
+
+// Array Splice
+Animal.splice(1, 0,'Monkey');
+console.log(Animal);
+// Result are Elephant, Monkey, Fish, Shark, Piranha
+// Splice means that we gonna insert new value at index 1 where there is nothing changes value
+// 1 means that index of 1 replaced and it will given after position of index 0
+// Result are Fish will remain there but the order value index from 1 to 2
+Animal.splice(2, 1, 'Cat');
+console.log(Animal);
+// Result are Elephant, Monkey, Cat, Shark, Piranha
+// Index of 2 will be inserted with new value called Cat
+// But it will replace current value of Fish into Cat
+// 2 means that index of 2 replaced and it will given after position index  1
+// This is how array splice work
+
+// Array Slice
+console.log(Animal.slice(1));
+// Result are Monkey, Cat, Shark, Piranha
+console.log(Animal.slice(2));
+// Result are Cat, Shark, Piranha
+console.log(Animal.slice(3));
+// Result are Shark, Piranha
+// This is how slice work on specific index order
+
+// Array Join
+console.log(Animal.join(' + '));
+// Result are Elephant + Monkey + Cat + Shark + Piranha
+console.log(Animal.join(' % '));
+// Result are Elephant % Monkey % Cat % Shark % Piranha
+// This is how array join work
