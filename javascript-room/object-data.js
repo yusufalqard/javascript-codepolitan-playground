79b1b327d0b8ab789cd0ce3aa22927c9f80f6618
yusufalqard <yusuf.alqard@gmail.com>
2025-01-19 11:01:00 +0700

    add: object data from line 1 to 11

---
+++ javascript-room/object-data.js
@@ -0,0 +1,11 @@
+// Object Data in Javascript
+// Here is the example of object data
+
+const Heartbeat = {
+    Pulse : 125,
+    BloodPressure : 70,
+    Activity_Level : 30,
+    BadCondition : false,
+    DataPerson : ['159cm','55kg','Male'],
+};
+console.log(Heartbeat);
