79949b5336310938d98583d1f65ed8d5f1ee686b
yusufalqard <yusuf.alqard@gmail.com>
2025-01-19 11:01:47 +0700

    add: explanation and how object data work in line 13 to 29

--- javascript-room/object-data.js
+++ javascript-room/object-data.js
@@ -9,3 +9,21 @@ const Heartbeat = {
     DataPerson : ['159cm','55kg','Male'],
 };
 console.log(Heartbeat);
+
+// If you want create more than 1 data, the prompt should be like this
+// const NameFunction = {
+// ObjectName : 'insert something here' and coma (,)
+// ObjectName : Number,
+// };
+
+// It also support with Array, Boolean, etc
+console.log(Heartbeat.Pulse);
+console.log(Heartbeat.BloodPressure);
+console.log(Heartbeat.Activity_Level);
+console.log(Heartbeat.BadCondition);
+console.log(Heartbeat.DataPerson);
+// This is right way to receive object data in javascript
+console.log(Heartbeat['DataPerson']);
+// This is wrong way to received object data in javascript
+// In Unorthodox way, this way used only in Array which not recommended to use this method
+// Array and Object has own their usage for development app
