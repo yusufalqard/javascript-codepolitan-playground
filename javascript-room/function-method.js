b1f1be7a63517628a047f76868d88f43c4d7223e
yusufalqard <yusuf.alqard@gmail.com>
2025-01-20 20:59:12 +0700

    fix: revision of function-method file

--- javascript-room/function-method.js
+++ javascript-room/function-method.js
@@ -1,4 +1,8 @@
 // Function and Method section
+// Here is how to remember how to use function
+// function Name_of_function () {
+//      code will processed
+// }
 
 function RollDice(){
     console.log(Math.floor(Math.random()*6) +1);
