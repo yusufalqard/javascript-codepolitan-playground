b09fc61fbd4980503ba15d0aaac7b5227edd9e1e
yusufalqard <yusuf.alqard@gmail.com>
2025-01-20 13:47:47 +0700

    add: looping state code from line 1 to 19

---
+++ javascript-room/looping-room.js
@@ -0,0 +1,18 @@
+// This section are part of looping in javascript
+// Basic knowledge for Looping
+
+// Example Looping for Easy Level
+
+// for (let i = 1; i <= 20; i++)
+// Result are 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+
+for (let i = 20; i >= 0; i -= 2)
+// Result are 20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0
+
+// for (let i = 10; i <= 20; i += 2)
+// Result are 10, 12, 14, 16, 18, 20
+
+// Enable/Disable the code to see the result
+    {
+    console.log(i);
+}
