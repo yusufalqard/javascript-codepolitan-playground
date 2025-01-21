c990bd0091a107d57ce4fded7d62b2df26f25ce7
yusufalqard <yusuf.alqard@gmail.com>
2025-01-21 11:02:53 +0700

    add: argument and parameter code at line 12 to 26

--- javascript-room/function-method.js
+++ javascript-room/function-method.js
@@ -8,3 +8,19 @@ function RollDice(){
     console.log(Math.floor(Math.random()*6) +1);
 }
 RollDice();
+
+// Argument and Paramater
+function Greeting(MyName){
+    console.log(`Hello there!, ${MyName}`);
+}
+Greeting("Yusuf");
+// How to remember
+// Greeting("Yusuf") where "Yusuf" is argument
+// function Greeeting() are considered as paramater
+
+// You can also use multiple paramater and argument together
+function AddNumeric(a,b){
+    const total = a + b;
+    console.log(`Result a + b is ${total}`);
+}
+AddNumeric(2,8);
