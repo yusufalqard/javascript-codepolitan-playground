1a566fb2f3ab2d030bf929d05db97d0d873aead5
yusufalqard <yusuf.alqard@gmail.com>
2025-01-24 11:17:45 +0700

    add: default value parameter at line 1 to 25

---
+++ javascript-room/bonus-javascript-module.js
@@ -0,0 +1,25 @@
+// Default Value Parameter
+// In high level programming languages, we write code like this
+function multiply (a,b = 1){
+    return a * b;
+}
+console.log(multiply(5,2)); // 10
+// In old level programming languages, we write code like this
+function multiply (a,b){
+    b = typeof b !== 'undefined' ? b : 1;
+    return a * b;
+}
+
+function RollMonopolyDice (numericDice){
+    return Math.floor(Math.random() * numericDice) + 1;
+}
+console.log(RollMonopolyDice(6)); // 1 to 6
+
+function SayHello (name, msg){
+    console.log(`${name}, ${msg}`);
+}
+console.log(SayHello());
+// Result are undefined, undefined
+// SayHello("John", "Good Morning");
+// Result are John, Good Morning
+// Enable/Disable code of comment at line 21 and 23 to see the result in console.log on line 19 with Quokka.js or Browser Console
