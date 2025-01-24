57bc3c01d5067bde2cf63e99ffee9971ce98ab4a
yusufalqard <yusuf.alqard@gmail.com>
2025-01-24 19:56:09 +0700

    add: destructuring with operator javascript at line 71 to 165

--- javascript-room/bonus-javascript-module.js
+++ javascript-room/bonus-javascript-module.js
@@ -67,3 +67,99 @@ const titleWinner = (gold, silver, bronze) => {
 }
 titleWinner(...OylmpicWinner);
 // Result are Gold : Usain Bolt, Silver : Michael Phelps, Bronze : Simone Biles
+
+// Destructuring in Javascript
+// Destructuring is used to extract array elements or object properties
+// First of all, we will use Destructuring with Array first
+
+// I am using exmaple of OylmpicWinner array at line 64
+const [a,b,c] = [OylmpicWinner];
+console.log(a,b,c);
+// Result are Usain Bolt Michael Phelps Simone Biles, Undefined and Undefined
+// Because b and c are not defined in array before at line 76
+// Where it should be [OylmpicWinner, ... , ... ] if we want to use b and c
+
+// Destructuring with Object in Javascript
+// This is example of Deconstructing with Object
+// const userStatus = {
+//     FullName : "Monica Galindo",
+//     Email : "moga@gmail.com",
+//     Role : "Admin",
+// };
+// const {FullName : realName , Email : registeredEmail, Role : jobDesk, phone = '555-0001'} = userStatus;
+// console.log(realName);
+// Result is Monica Galindo
+// console.log(registeredEmail);
+// Result is moga@gmail.com
+// console.log(jobDesk);
+// Result is Admin
+// console.log(phone);
+// Result is 555-0001
+
+// You can also use Destructuring with object inside of function
+// This is example of Deconstructing with Object inside of function
+// I am using example of userStatus object at line 84
+const userStatus = {
+    FullName : "Monica Galindo",
+    Email : "moga@gmail.com",
+    Role : "Admin",
+};
+
+const EmployeeRole = ({FullName, Role}) => {
+    return `${FullName} is ${Role}`;
+};
+EmployeeRole(userStatus);
+console.log(EmployeeRole(userStatus));
+// Result is Monica Galindo is Admin
+
+// Remember in the day about Movie Title?
+// We can use Destructing method too in this case
+const MovieTitle = [
+    {
+    Title : "The Matrix",
+    Year : 1999,
+    Director : "Lana Wachowski",
+    Rating : 8.7,
+    },
+    {
+    Title : "The One",
+    Year : 2001,
+    Director : "James Wong",
+    Rating : 8.1,
+    },
+    {
+    Title : "Project Almanac",
+    Year : 2015,
+    Director : "Almanac",
+    Rating : 7.3,
+    },
+    {
+    Title : "Eagle Eye",
+    Year : 2008,
+    Director : "D.J. Caruso",
+    Rating : 9.1,
+    },
+];
+
+// First of thing you need to flashback with callback function
+const Movie = MovieTitle.map((Movie) => {
+    return `${Movie.Title} is directed by ${Movie.Director} at ${Movie.Year}`;
+});
+console.log(Movie);
+// Result are ["The Matrix is directed by Lana Wachowski at 1999", "The One is directed by James Wong at 2001", "Project Almanac is directed by Almanac at 2015", "Eagle Eye is directed by D.J. Caruso at 2008"]
+// However, it really less effective
+// So in this case, we use Destructuring method
+
+const BestMovie = MovieTitle.map(({Title, Director, Year}) => {
+    return `${Title} from ${Director} at ${Year}`;
+});
+console.log(BestMovie);
+// Result are ["The Matrix from Lana Wachowski at 1999", "The One from James Wong at 2001", "Project Almanac from Almanac at 2015", "Eagle Eye from D.J. Caruso at 2008"]
+// You can see the different between line 145 and line 153
+// Which is more efficient and understandable
+
+
+// END of SECTION Javascript ROOM - BONUS JAVASCRIPT MODULE
+// This is part of end material of Learn Javascript - Beginner level
+
+// Please check onto next folder Object-Oriented Programming with Javascript
