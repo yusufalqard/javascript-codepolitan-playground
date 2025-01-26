192237562c55d1abbe79c2170b388471bea04133
yusufalqard <yusuf.alqard@gmail.com>
2025-01-26 17:16:13 +0700

    add: class on OOP javascript at line 110 to 178

--- oop-javascript-room/app.js
+++ oop-javascript-room/app.js
@@ -65,27 +65,27 @@ function convertColor(r, g, b) {
 // Create Object with Constructor Function
 // Constructor function is a function that is used to create an object.
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
-}
-Color.prototype.rgb = function () {
-  const { r, g, b } = this;
-  return `rgb(${r}, ${g}, ${b})`;
-};
-Color.prototype.hex = function () {
-  const { r, g, b } = this;
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
-// So, we can call the function Color like this
-// Input in browser console this code below
-// new Color(255, 100, 25).hex()
-// Result is #ff6419
-Color.prototype.rgba = function (a = 1.0) {
-    const {r,g,b} = this;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+// // So, we can call the function Color like this
+// // Input in browser console this code below
+// // new Color(255, 100, 25).hex()
+// // Result is #ff6419
+// Color.prototype.rgba = function (a = 1.0) {
+//     const {r,g,b} = this;
+//     return `rgba(${r}, ${g}, ${b}, ${a})`;
+// }
 // Input in browser console this code below
 // document.body.style.backgroundColor = new Color (255, 100, 25).rgba(0.5);
 // Result is rgba(255, 100, 25, 0.5)
@@ -106,3 +106,73 @@ Color.prototype.rgba = function (a = 1.0) {
 // Result are true
 // Method color on Color will have shared method between object and it will be the same everytime we input new value
 // Its belong to the prototype of the object source location
+
+// Create Object with Class on OOP Javascript
+// Class is a blueprint for creating objects with pre-defined properties and methods.
+// Class is a template for creating objects.
+
+// class Color {
+//     constructor(r,g,b){
+//         // Constructor is a method that is called when an object is created.
+//         // It is used to initialize the object's properties.
+
+//         this.r = r;
+//         this.g = g;
+//         this.b = b;
+//         console.log("Testing constructor");
+//         console.log(`${r}, ${g}, ${b}`);
+//     }
+// }
+// Input in browser console to test the code
+// Color
+// Result are detail of class Color
+// new Color()
+// Result are Testing constructor and undefined, undefined, undefined
+// new Color(255,14,11)
+// Result are Testing constructor and 255, 14, 11
+
+// However there is a better way to create new constructor for this one
+class Color {
+    constructor(r,g,b,name){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    innerRGB(){
+        const {r,g,b} = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+    colorName(){
+        console.log(`This color is ${this.name}`);
+    }
+    rgb(){
+        return `rgb(${this.innerRGB()})`;
+    }
+    hex(){
+        const {r,g,b} = this;
+        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    rgba(a = 1.0){
+        return `rgba(${this.innerRGB()}, ${a})`;
+    }
+}
+// Clearly this is how to create a class with constructor and method inside the class
+// WIthout writing same method for each object created
+
+// Input in browser console to test the code
+// const SkyColor = new Color(185,243,252, 'Sky Blue');
+// Initialize the object SkyColor with new Color(185,243,252, 'Sky Blue')
+// SkyColor
+// Result are detail of class Color
+// SkyColor.rgb()
+// Result are rgb(185, 243, 252)
+
+// Now try this to change style of element html
+// document.body.style.backgroundColor = SkyColor.rgb()
+// or
+// document.body.style.backgroundColor = SkyColor.hex()
+// or
+// document.body.style.backgroundColor = SkyColor.rgba(0.5)
+// Your HTML Body color changes to Sky Blue depend which element code you input before
+// either rgb, hex, or rgba
