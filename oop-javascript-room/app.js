bd95f20162265050d0a5d6aa07c9f8fb021c9a7a
yusufalqard <yusuf.alqard@gmail.com>
2025-01-26 15:55:37 +0700

    add: factory function with object at line 20 to 63

--- oop-javascript-room/app.js
+++ oop-javascript-room/app.js
@@ -6,13 +6,58 @@
 // OOP is a way to structure the code in a more organized way.
 
 // Example of OOP in JavaScript - Introduction
-console.log([1,2,3].length);
-console.log([1,2,3].pop);
+// console.log([1,2,3].length);
+// console.log([1,2,3].pop);
 // Result are 3 and function pop() { [native code] }
-const Arr = [1,2,3];
-Arr.hello = () => {console.log('Hello')};
-console.log(Arr.hello());
+// const Arr = [1,2,3];
+// Arr.hello = () => {console.log('Hello')};
+// console.log(Arr.hello());
 // Result is Hello if you run the script in the browser console.
 // But if you run the script in the terminal, the result is undefined.
 // Because the array is an object in JavaScript, so you can add a property to the object.
 // But the property is not a part of the array, it is a part of the object.
+
+// Create Object with Factory Function
+// const hex = (r,g,b) => {
+//     return '#' + ((1<<24) + (r<<16) + (g<<8) + b).toString(16).slice(1);
+// };
+// const rgb =  (r,g,b) => {
+//     return `rgb(${r}, ${g}, ${b})`;
+// };
+// function convertColor (r,g,b) {
+//     const color = {};
+//     color.r = r
+//     color.g = g
+//     color.b = b
+
+//     return color;
+// }
+
+// This is code less effective
+// Therefore we could make those code much better shown below here
+
+function convertColor (r,g,b) {
+    const color = {};
+    color.r = r
+    color.g = g
+    color.b = b
+    
+    color.rgb = function () {
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    color.hex = function () {
+        const {r, g, b} = this;
+        return '#' + ((1<<24) + (r<<16) + (g<<8) + b).toString(16).slice(1);
+    };
+
+    return color;
+}
+
+// So, we can call the function convertColor like this
+// Input in browser console this code below
+// const color = convertColor(255, 100, 25);
+// color.rgb();
+// result is rgb(255, 100, 25);
+// color.hex();
+// result is #ff6419;
+// It also works with Array, Object, and Function.
