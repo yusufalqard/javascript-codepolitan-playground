5e1c98244bb8ba9efeaaf1824de5766795c28015
yusufalqard <yusuf.alqard@gmail.com>
2025-01-26 14:46:34 +0700

    add: oop javascript intro at line 1 to 15

---
+++ oop-javascript-room/app.js
@@ -0,0 +1,15 @@
+// Object-Oriented Programming (OOP) in JavaScript
+// OOP is a programming paradigm that relies on the concept of classes and objects. 
+// It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.
+// Which makes it easier to manage and maintain the code.
+// Without OOP, the code can be messy and hard to maintain.
+// OOP is a way to structure the code in a more organized way.
+
+// Example of OOP in JavaScript - Introduction
+console.log([1,2,3].length);
+console.log([1,2,3].pop);
+// Result are 3 and function pop() { [native code] }
+const Arr = [1,2,3];
+Arr.hello = () => {console.log('Hello')};
+console.log(Arr.hello());
+// Result is Hello if you run the script in the browser console.
