4d6cf3db12a82b73fc26a267b1dd382f825a0d59
yusufalqard <yusuf.alqard@gmail.com>
2025-01-26 20:22:44 +0700

    add: more technique with create object of class OOP at line 180 to 269

--- oop-javascript-room/app.js
+++ oop-javascript-room/app.js
@@ -132,31 +132,31 @@ function convertColor(r, g, b) {
 // Result are Testing constructor and 255, 14, 11
 
 // However there is a better way to create new constructor for this one
-class Color {
-    constructor(r,g,b,name){
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.name = name;
-    }
-    innerRGB(){
-        const {r,g,b} = this;
-        return `rgb(${r}, ${g}, ${b})`;
-    }
-    colorName(){
-        console.log(`This color is ${this.name}`);
-    }
-    rgb(){
-        return `rgb(${this.innerRGB()})`;
-    }
-    hex(){
-        const {r,g,b} = this;
-        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    }
-    rgba(a = 1.0){
-        return `rgba(${this.innerRGB()}, ${a})`;
-    }
-}
+// class Color {
+//   constructor(r, g, b, name) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+//     this.name = name;
+//   }
+//   innerRGB() {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   }
+//   colorName() {
+//     console.log(`This color is ${this.name}`);
+//   }
+//   rgb() {
+//     return `rgb(${this.innerRGB()})`;
+//   }
+//   hex() {
+//     const { r, g, b } = this;
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   }
+//   rgba(a = 1.0) {
+//     return `rgba(${this.innerRGB()}, ${a})`;
+//   }
+// }
 // Clearly this is how to create a class with constructor and method inside the class
 // WIthout writing same method for each object created
 
@@ -176,3 +176,94 @@ class Color {
 // document.body.style.backgroundColor = SkyColor.rgba(0.5)
 // Your HTML Body color changes to Sky Blue depend which element code you input before
 // either rgb, hex, or rgba
+
+// Create object with class - OOP Javascript level intermediate technique
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+    this.calcHSL();
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  colorName() {
+    console.log(`This color is ${this.name}`);
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
+  hsl(){
+    const {r,g,b} = this;
+    return `hsl(${this.h}, ${this.s}%, ${this.l}%)`;
+  }
+  fulllySaturated(){
+    const {h,l} = this;
+    return `hsl(${h}, 100%, ${l})`;
+  }
+  opposite(){
+    const {h,s,l} = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue}, ${s}%, ${l}%)`;
+  }
+
+  // Reference from https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsl
+  calcHSL() {
+    let { r, g, b } = this;
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r)
+      // Red is max
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      // Green is max
+      h = (b - r) / delta + 2;
+    // Blue is max
+    else h = (r - g) / delta + 4;
+    h = Math.round(h * 60);
+    // Make negative hues positive behind 360°
+    if (h < 0) h += 360;
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
+}
+// Input in browser console to test the code
+// const SkyColor = new Color(185,243,252, 'Sky Blue');
+// Initialize the object SkyColor with new Color(185,243,252, 'Sky Blue')
+
+// document.body.style.backgroundColor = SkyColor.hsl()
+// Result is hsl(195, 86.3%, 86.3%)
+// document.body.style.backgroundColor = SkyColor.fulllySaturated()
+// Result is hsl(195, 100%, 86.3%)
+// document.body.style.backgroundColor = SkyColor.opposite()
+// Result is hsl(15, 86.3%, 86.3%)
+
+// This is how to create a class with constructor for intermediate level
