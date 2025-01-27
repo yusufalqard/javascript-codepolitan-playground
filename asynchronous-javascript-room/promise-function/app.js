c0737afab3f296693c91bcdde3fa2f6de9db6611
yusufalqard <yusuf.alqard@gmail.com>
2025-01-27 16:55:14 +0700

    add: promise func for async javascript at line 15 to 27

--- asynchronous-javascript-room/promise-function/app.js
+++ asynchronous-javascript-room/promise-function/app.js
@@ -2,12 +2,26 @@
 
 // Callback Version
 const requestCallback = (url, success, failure) => {
-    const delay = Math.floor(Math.random()*4500) + 500;
-    setTimeout(()=>{
-        if(delay>4000){
-            failure(`Error: Connection Timeout`);
-        }else {
-            success(`Success: ${url}(${delay}ms)`);
-        }
+  const delay = Math.floor(Math.random() * 4500) + 500;
+  setTimeout(() => {
+    if (delay > 4000) {
+      failure(`Error: Connection Timeout`);
+    } else {
+      success(`Success: ${url}(${delay}ms)`);
+    }
+  }, delay);
+};
+
+// Promise version
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        failure(`Error: Connection Timeout`);
+      } else {
+        success(`Success: ${url}(${delay}ms)`);
+      }
     }, delay);
+  });
 };
