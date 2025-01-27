e897736b0c4f5330be92cf38ce389a2f0138c7f7
yusufalqard <yusuf.alqard@gmail.com>
2025-01-27 15:16:14 +0700

    add: single thread async javascript

---
+++ asynchronous-javascript-room/single-thread.js
@@ -0,0 +1,31 @@
+// Single Thread on Asynchronous Javascript
+// Single Thread are program that only run process one request at time
+
+// Example code
+const newTask = input.value; // Get user input from task input
+sendToServerAndSaveIt(newTask); // Input take process a while depends how many process taken by server
+input.value = ''; // Will process after save data
+
+// Example logic of your daily life in morning routine
+
+// 1. Wake up morning from bed
+// 2. Strectching your body after wake up for 5 minutes
+// 3. Preparing Food and Drinks materials for cooking
+// 4. Prep a bread, choco nutella jam and knife then combine all ingredients
+// 5. Prep a oven bread and set auto-off timer for 10 minutes
+// 6. Prep kettle and fresh water then cook in gas stove for 3 minutes, set on high flames
+// 7. Prep a Tea Sachet and Sugar
+// 8. Put all ingredients in big glass drink
+// 8. Turn off gas stove, pour hot water from kettle to glass drink
+// 10. Mix all ingredients until everyting dissolve
+// 11. Keep it outdoor for 4 minutes in table and prep towel, tooth paste, soap, and shampoo into bathroom
+// 12. Go wash yourself at bathroom for 7 minutes while your hot tea on cooling
+// 13. Use towel after bath then find your clothes
+// 14. Back to your kitchen, then prep a plate and take your cooked bread from oven
+// 15. Place those nearby with your tea glass drink
+// 16. Sit on chair and enjoy your breakfast meal
+
+// See how many process taken of this logic in real life
+// Everything was done in order same like Single Thread does in Asynchronous Javascript
+// If we want use this logic into code, you can actually do it.
+// Asynchronous function can help during build code process
