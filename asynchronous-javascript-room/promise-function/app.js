44486a27817d46d0297bb1d075dbcb53908e514d
yusufalqard <yusuf.alqard@gmail.com>
2025-01-29 13:17:43 +0700

    add: example callback hell function at line 29 to 79

--- asynchronous-javascript-room/promise-function/app.js
+++ asynchronous-javascript-room/promise-function/app.js
@@ -25,3 +25,55 @@ const requestPromise = (url) => {
     }, delay);
   });
 };
+
+// Example about callback function
+requestCallback('movie.com', function(response){
+  console.log(`success`,response);  
+},
+function(error){
+  console.log(`error`, error);
+});
+
+// Exampe why developer not using callback function
+
+// requestPromise('movie.com')
+// 	.then((response) => {
+// 		console.log('success', response);
+// 		requestPromise('movie.com')
+// 			.then((response) => {
+// 				console.log('success', response);
+// 				requestPromise('movie.com')
+// 					.then((response) => {
+// 						console.log('success', response);
+// 						requestPromise('movie.com')
+// 							.then((response) => {
+// 								console.log('success', response);
+// 								requestPromise('movie.com')
+// 									.then((response) => {
+// 										console.log('success', response);
+// 									})
+// 									.catch((error) => {
+// 										console.log('error', error);
+// 									});
+// 							})
+// 							.catch((error) => {
+// 								console.log('error', error);
+// 							});
+// 					})
+// 					.catch((error) => {
+// 						console.log('error', error);
+// 					});
+// 			})
+// 			.catch((error) => {
+// 				console.log('error', error);
+// 			});
+// 	})
+// 	.catch((error) => {
+// 		console.log('error', error);
+// 	});
+
+// This is why we do not use Callback function to prevent Callback hell
+// where we use callback function multiple time like IF ELSE nested
+// until they decide add limit for their code
+// As developer, you do not wanna break your code like this
+// or prevent this callback hell usage ever on code development
