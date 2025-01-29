66b882b09df2cc6795a555353eab91af35382043
yusufalqard <yusuf.alqard@gmail.com>
2025-01-29 15:50:27 +0700

    add: async and await function at line 18 to 48

--- asynchronous-javascript-room/backgroundChanger/app.js
+++ asynchronous-javascript-room/backgroundChanger/app.js
@@ -8,9 +8,41 @@ const delayColorChange = (color, delay) => {
     });
 };
 
-delayColorChange('red',1000)
-    .then(() => delayColorChange('yellow',1000))
-    .then(() => delayColorChange('green',1000))
-    .then(() => delayColorChange('blue',1000))
-    .then(() => delayColorChange('purple',1000))
-    .then(() => delayColorChange('magenta',1000));
+// delayColorChange('red',1000)
+//     .then(() => delayColorChange('yellow',1000))
+//     .then(() => delayColorChange('green',1000))
+//     .then(() => delayColorChange('blue',1000))
+//     .then(() => delayColorChange('purple',1000))
+//     .then(() => delayColorChange('magenta',1000));
+
+// Async and Await on Asynchronous Javascript
+
+// Async function works automatic when any changes happen on Promise call function
+// Here is the logic
+// If we have data function, then we can resolve by sending data
+// or even we can processing data at same time
+// If we have function that contain object throw erro inside of callback function
+// then we can sending reject promise to notified users about this
+
+// Await function works same as Async
+// But the difference is "Await" keyword only works if we have Async function
+// Any other Async function will not work as intended
+// Await always have delay process until next function assigned
+// By waiting and running Await until Async function resolved 
+
+// Here is the example of Async and Await of callback function
+async function changeColor () {
+    await delayColorChange('red',1000);
+    await delayColorChange('yellow',1000);
+    await delayColorChange('green',1000);
+    await delayColorChange('blue',1000);
+    await delayColorChange('purple',1000);
+    await delayColorChange('magenta',1000);
+    return 'All color changes success';
+}
+
+async function printRainbow (){
+    await changeColor();
+    console.log('All Process Done! Function printRainbow process executed')
+}
+printRainbow();
