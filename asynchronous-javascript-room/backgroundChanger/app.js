66bdbff4ddac6f6772f8b9cc0eb5679b9e102795
yusufalqard <yusuf.alqard@gmail.com>
2025-01-29 14:58:05 +0700

    add: delayColorChange on html using promise at line 2 to 16

--- asynchronous-javascript-room/backgroundChanger/app.js
+++ asynchronous-javascript-room/backgroundChanger/app.js
@@ -1,1 +1,16 @@
 // Initialize backgroundChanger script
+const delayColorChange = (color, delay) => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay);
+    });
+};
+
+delayColorChange('red',1000)
+    .then(() => delayColorChange('yellow',1000))
+    .then(() => delayColorChange('green',1000))
+    .then(() => delayColorChange('blue',1000))
+    .then(() => delayColorChange('purple',1000))
+    .then(() => delayColorChange('magenta',1000));
