84140fa8782119cdd3ffaba8040c97e8c965a3e4
yusufalqard <yusuf.alqard@gmail.com>
2025-02-03 13:57:52 +0700

    add: try catch method for fetch API json

--- ajax-web-api-room/GetDaddyJokes/app.js
+++ ajax-web-api-room/GetDaddyJokes/app.js
@@ -1,21 +1,25 @@
-const jokes = document.querySelector('#jokes');
-const button = document.querySelector('button');
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
 
 const addDadJokes = async () => {
-    const textJoke = await getDadJokes();
-    const newLI = document.createElement('LI');
-    newLI.append(textJoke);
-    jokes.append(newLI);
+  const textJoke = await getDadJokes();
+  const newLI = document.createElement("LI");
+  newLI.append(textJoke);
+  jokes.append(newLI);
 };
 
 const getDadJokes = async () => {
-  const SetConfig = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
-  const res = await axios.get("https://icanhazdadjoke.com/", SetConfig);
-  return(res.data.joke);
+  try {
+    const SetConfig = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await axios.get("https://icanhazdadjoke.com/", SetConfig);
+    return res.data.joke;
+  } catch (error) {
+    return 'No Jokes obtained';
+  }
 };
 
-button.addEventListener('click', addDadJokes);
+button.addEventListener("click", addDadJokes);
