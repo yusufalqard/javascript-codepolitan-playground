cac0d8f4d70f19ad7ddc5123e01e8f2bfc2e469b
yusufalqard <yusuf.alqard@gmail.com>
2025-02-03 13:08:54 +0700

    add: fetch method usage for API parse json

--- ajax-web-api-room/Ajax-API/app.js
+++ ajax-web-api-room/Ajax-API/app.js
@@ -1,14 +1,43 @@
-const DataTestRequest = new XMLHttpRequest();
-let ReqData;
+// Using XMLHttpRequest method
 
-DataTestRequest.onload = function () {
-    const ReqData = JSON.parse(this.responseText);
-    console.log(ReqData);
-};
+// const DataTestRequest = new XMLHttpRequest();
+// let ReqData;
 
-DataTestRequest.onerror = function () {
-    console.log('Error', this);
-};
+// DataTestRequest.onload = function () {
+//     const ReqData = JSON.parse(this.responseText);
+//     console.log(ReqData);
+// };
 
-DataTestRequest.open('GET', 'https://swapi.dev/api/people/2');
-DataTestRequest.send();
+// DataTestRequest.onerror = function () {
+//     console.log('Error', this);
+// };
+
+// DataTestRequest.open('GET', 'https://swapi.dev/api/people/2');
+// DataTestRequest.setRequestHeader('Accept','application/json');
+// DataTestRequest.send();
+
+// Using Fetch method
+fetch("https://swapi.dev/api/people/3")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Could not fetch the data for that resource");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log("1st JSON", data);
+    return fetch("https://swapi.dev/api/people/4");
+  })
+  .then((res) => {
+    console.log("2nd JSON");
+    if (!res.ok) {
+      throw Error("Could not fetch the data for that resource");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
